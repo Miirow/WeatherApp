32ba2df4b7dd6ee26185edd5355c6019017ff3b4
Mathéo Délié <matheodelie@gmail.com>
2024-12-12 00:34:30 +0100

    V2.0.0

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+const API_KEY = '7e977715948c7c7738668da30ec1ffa1';
+
 document.getElementById('search-form').addEventListener('submit', async function (e) {
     e.preventDefault();
     const city = document.getElementById('city-input').value;
@@ -17,7 +19,6 @@ document.getElementById('search-form').addEventListener('submit', async function
 
         const data = await response.json();
         updateWeatherInfo(data);
-        adjustCloudSpeed(data.wind.speed); // Ajoute la fonction pour ajuster la vitesse des nuages
     } catch (error) {
         alert(error.message);
     }
@@ -29,18 +30,10 @@ function updateWeatherInfo(data) {
     document.getElementById('humidity').textContent = `${data.main.humidity} % Humidité`;
     document.getElementById('wind-speed').textContent = `${Math.round(data.wind.speed)} km/h Vitesse du vent`;
 
+
     const iconCode = data.weather[0].icon;
     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
     const weatherIcon = document.getElementById('weather-icon');
     weatherIcon.src = iconUrl;
     weatherIcon.style.display = 'block';
-}
-
-function adjustCloudSpeed(windSpeed) {
-    // Calculer la durée de l'animation en fonction de la vitesse du vent
-    let speedFactor = Math.max(6, 20 - windSpeed * 2); // Ajuster entre 6s et 20s
-    console.log(`Vitesse du vent : ${windSpeed} m/s, durée de l'animation : ${speedFactor}s`);
-
-    // Appliquer la durée calculée à la variable CSS
-    document.documentElement.style.setProperty('--cloud-speed', `${speedFactor}s`);
 }
